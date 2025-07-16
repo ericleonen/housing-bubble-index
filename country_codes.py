f94cedf8c596d4787010f5e2bd45aee347ef1306
ericleonen <ericleonen@gmail.com>
2025-07-16 11:31:36 -0700

    create pipeline for BIS real RPP YoY data

---
+++ country_codes.py
@@ -0,0 +1,13 @@
+CODE_TO_COUNTRY = {
+    "BE": "Belgium",
+    "CA": "Canada",
+    "CH": "Switzerland",
+    "DE": "Denmark",
+    "FR": "France",
+    "GB": "United Kingdom",
+    "IT": "Italy",
+    "JP": "Japan",
+    "NL": "Netherlands",
+    "SE": "Sweden",
+    "US": "United States"
+}
